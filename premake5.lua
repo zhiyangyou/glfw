75696aa8efde04cbdc2d0a02bf9677cfc38602cb
youzhiyang <1454900087@qq.com>
2023-08-13 20:51:26 +0800

    修改premake

--- premake5.lua
+++ premake5.lua
@@ -76,21 +76,23 @@ project "GLFW"
 		}
 
 	filter "system:windows"
-		systemversion "latest"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "latest"
+        staticruntime "On"
+        
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
 		defines 
 		{ 
